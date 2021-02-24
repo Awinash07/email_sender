57f1d4786518f791312bb6cd34214a2071a9dea5
Awinash Awasthy <awinashawasthy@gmail.com>
2021-02-24 09:33:38 +0530

    my first commi

---
+++ email_sender.py
@@ -0,0 +1,20 @@
+import smtplib
+from email.message import EmailMessage
+from string import Template
+from pathlib import Path
+
+html = Template(Path("index.html").read.text())
+email = EmailMessage()
+email["from"] = "Awinash Awasthy"
+email["to"] = "awinashawasthy@outlook.com"
+email["subject"] = "you won 1lac rs!!"
+
+email.set_content(html.substitute({"name":"tintin"}),html)
+
+with smtplib.SMTP(host="smtp.gmail.com",port=587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login("awinashawasthy@gmail.com","hggfd")
+    smtp.send_message()
+    print("all good")
+    
